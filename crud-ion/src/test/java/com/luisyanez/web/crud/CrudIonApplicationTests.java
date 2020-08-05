87c204815cc5a130baf8ea44166cbc42548751ed
luxox <luxox@DESKTOP-TQJE0N5>
2020-08-05 17:19:01 -0400

    Primer Version

---
+++ crud-ion/src/test/java/com/luisyanez/web/crud/CrudIonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisyanez.web.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudIonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
